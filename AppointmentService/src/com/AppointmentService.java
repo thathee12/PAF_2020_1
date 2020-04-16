74f2b458379558b5c54c9cbbe4802b75f8fbec75
thathee12 <twickramaarachchi@yahoo.com>
2020-04-17 03:20:49 +0530

    Insert appoinment function added

--- AppointmentService/src/com/AppointmentService.java
+++ AppointmentService/src/com/AppointmentService.java
@@ -1,5 +1,24 @@
 package com;
 
+import java.sql.Date;
+
+//For REST Service 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+//For JSON 
+import com.google.gson.*;
+
+import model.Appointment;
+
+
+//For XML 
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+
+@Path("/Appointment")
 public class AppointmentService {
 
 }
--- AppointmentService/src/model/Appointment.java
+++ AppointmentService/src/model/Appointment.java
@@ -1,5 +1,33 @@
 package model;
 
-public class Appointment {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
 
+import java.util.Date;
+
+
+
+public class Appointment {
+	
+	// A common method to connect to the DB
+		private Connection connect() {
+			Connection con = null;
+
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				// Provide the correct details: DBServer/DBName, username, password
+				con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf_ca", "root", "1Thathi/");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			return con;
+		}
+		
+		
+		
 }

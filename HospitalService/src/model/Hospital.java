11bac69cba9bef26b093f026ad54c0e34701e088
SathKav <sathsaranika@gmail.com>
2020-04-17 02:45:52 +0530

    connection class created

--- HospitalService/src/model/Hospital.java
+++ HospitalService/src/model/Hospital.java
@@ -1,6 +1,22 @@
 package model;
 
-//this is model class 
-public class Hospital {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class Hospital {
+	//connection class created
+		private Connection connect()
+		 {
+		 Connection con = null;
+		 try
+		 {
+		 Class.forName("com.mysql.jdbc.Driver");
+
+		 //Provide the correct details: DBServer/DBName, username, password
+		 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf_ca", "root", "8172429Amma*");
+		 }
+		 catch (Exception e)
+		 {e.printStackTrace();}
+		 return con;
+		 } 
 }

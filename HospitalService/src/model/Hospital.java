46e1c8b07e847a2baf1b04be91c3c8beeddc5cbf
SathKav <sathsaranika@gmail.com>
2020-04-18 00:41:11 +0530

    Hospital branch delete function created

--- HospitalService/src/model/Hospital.java
+++ HospitalService/src/model/Hospital.java
@@ -152,4 +152,28 @@ public class Hospital {
 			return output;
 		}
 		
+		//delete operation
+		public String deletehosbranch(String hosID) {
+			String output = "";
+			try {
+				Connection con = connect();
+				if (con == null) {
+					return "Error while connecting to the database for deleting.";
+				}
+				// create a prepared statement
+				String query = "delete from hospital where hosID=?";
+				PreparedStatement preparedStmt = con.prepareStatement(query);
+				// binding values
+				preparedStmt.setInt(1, Integer.parseInt(hosID));
+				// execute the statement
+				preparedStmt.execute();
+				con.close();
+				output = "Deleted successfully";
+			} catch (Exception e) {
+				output = "Error while deleting the item.";
+				System.err.println(e.getMessage());
+			}
+			return output;
+		}
+		
 }

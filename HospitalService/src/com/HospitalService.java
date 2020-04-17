6f91427a72c619d1ff300073bfeef99e4bbff19d
SathKav <sathsaranika@gmail.com>
2020-04-18 00:53:08 +0530

    GET method added to HospitalService

--- HospitalService/src/com/HospitalService.java
+++ HospitalService/src/com/HospitalService.java
@@ -1,12 +1,27 @@
 package com;
 
-import javax.ws.rs.Path;
-
 import model.Hospital;
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
 
 @Path("/Hospitals")
 
 
 public class HospitalService {
 	Hospital hosObj = new Hospital();
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readhosbranch()
+	 {
+	 return hosObj.readhosbranch();
+	 } 
 }

09dde1cc9c7744a6ceee7f06cdc7a2c3ac6f83f7
Piumi <piumi.sulochana1212@gmail.com>
2020-04-18 14:11:11 +0530

    Edited the read function

--- ScheduleService/src/com/ScheduleService.java
+++ ScheduleService/src/com/ScheduleService.java
@@ -39,7 +39,7 @@ public class ScheduleService {
 	@GET
 	@Path("/ScheduleDetails")
 	@Produces(MediaType.TEXT_HTML)
-	public String readSchedule() {
+	public String readAllTypes() {
 		return schedule.readSchedule();
 	}
 

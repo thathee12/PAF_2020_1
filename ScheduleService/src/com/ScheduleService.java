8b5e31a07ae36ffc818ec902c918c4e5652f9a7c
Piumi <piumi.sulochana1212@gmail.com>
2020-04-18 14:15:29 +0530

    Updated the insert function

--- ScheduleService/src/com/ScheduleService.java
+++ ScheduleService/src/com/ScheduleService.java
@@ -44,6 +44,23 @@ public class ScheduleService {
 		return schedule.readSchedule();
 	}
 	
-	
+	// add Schedule
+			@POST
+			@Path("/ScheduleDetails")
+			@Consumes(MediaType.APPLICATION_JSON)
+			@Produces(MediaType.TEXT_PLAIN)
+			public String enterType(String ScheduleData) {
+			// Convert the input string to a JSON object
+			JsonObject jsonObject = new JsonParser().parse(ScheduleData).getAsJsonObject();
+
+			// Read the values from the JSON object
+			int docId =jsonObject.get("docId").getAsInt();
+			String workingDay = jsonObject.get("workingDay").getAsString();
+			int max_num_of_patients =jsonObject.get("max_num_of_patients").getAsInt();
+						
+			String output = schedule.addSchedule(docId, workingDay, max_num_of_patients);
+			return output;
+
+		}
 
 }

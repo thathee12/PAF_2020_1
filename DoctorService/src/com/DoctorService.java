1f3c93f46af81b54f5a7cbe33368facc1b627e7a
Piumi <piumi.sulochana1212@gmail.com>
2020-04-18 13:16:03 +0530

    Added read function

--- DoctorService/src/com/DoctorService.java
+++ DoctorService/src/com/DoctorService.java
@@ -1,5 +1,45 @@
 package com;
 
+import java.sql.Date;
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Timer;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import model.Doctor;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Time;
+
+@Path("/Doctors")
 public class DoctorService {
 
+Doctor doctor = new Doctor();
+	
+	// read Doctors 
+		@GET
+		@Path("/DoctorDetails")
+		@Produces(MediaType.TEXT_HTML)
+		public String readAllTypes() {
+			return doctor.readDoctor();
+		}
 }
--- DoctorService/src/model/Doctor.java
+++ DoctorService/src/model/Doctor.java
@@ -28,4 +28,65 @@ public class Doctor {
 
 		return con;
 	}
+	
+	//------------------------- Read details from table -------------------------
+	
+		public String readDoctor() {
+
+			String output = "";
+			try {
+				Connection con = connect();
+				if (con == null) {
+					return "Error while connecting to the database for reading.";
+				}
+				
+				//preparing the HTML table to be displayed 
+				output = "<table border=\"1\"><tr><th>DocId</th>"
+						+ "<th>Fname</th>"
+						+ "<th>Lname</th>"
+						+ "<th>Specialization</th>"
+						+ "<th>Contact</th>"
+						+ "<th>Email</th>"
+						+ "<th>Address</th>"
+						+ "<th>Gender</th>"
+						+ "<th>DocCharge</th>"
+						+ "<th>Hospital</th>"
+						+ "<th>Hospital</th></tr>";
+				
+				String query = "SELECT * FROM doctor";
+				
+				Statement statement = con.createStatement();
+				ResultSet resultSet = statement.executeQuery(query);
+				
+				//iterate through the rows in the result set			
+				while (resultSet.next()) {
+					int docId =    resultSet.getInt("docId");
+					String docFname =  resultSet.getString("docFname");
+					String docLname =  resultSet.getString("docLname");
+					String specialization =  resultSet.getString("specialization");
+					int contact =  resultSet.getInt("contact");
+					String email =  resultSet.getString("email");
+					String address =  resultSet.getString("address");
+					String gender =  resultSet.getString("gender");
+					float docCharge = resultSet.getFloat("docCharge");
+					int hospital =  resultSet.getInt("hospital");
+					String passowrd =  resultSet.getString("password");
+					
+		
+					//add into the HTML table
+					output += "<tr><td>" + docId + "</td><td>" + docFname + "</td><td>" + docLname + "</td><td>" + specialization + "</td><td>" + contact + "</td><td>" + email + "</td><td>" + address + "</td><td>" + gender + "</td><td>" + docCharge + "</td><td>" + hospital + "</td><td>" + passowrd + "</td></tr>";
+
+				}
+
+				con.close();
+				
+				output += "</table>";
+
+			} catch (Exception e) {
+				output = "Error while reading the Doctors";
+				System.err.println(e.getMessage());
+			}
+
+			return output;
+		}
 }

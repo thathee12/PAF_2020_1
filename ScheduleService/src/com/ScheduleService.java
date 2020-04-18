7e9799b68bdd5e964cb0ef20e36a2f05007c0099
Piumi <piumi.sulochana1212@gmail.com>
2020-04-18 14:08:06 +0530

    Removed the insertion function

--- ScheduleService/src/com/ScheduleService.java
+++ ScheduleService/src/com/ScheduleService.java
@@ -1,5 +1,35 @@
 package com;
 
+import java.sql.Date;
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Timer;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import model.Schedule;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Time;
+
 public class ScheduleService {
 
 }

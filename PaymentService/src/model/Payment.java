ed63854748cf669df66bf83e5ed2919b181a1593
Accash <accashr4@gmail.com>
2020-04-18 16:02:26 +0530

    test

--- PaymentService/src/model/Payment.java
+++ PaymentService/src/model/Payment.java
@@ -1,5 +1,18 @@
 package model;
 
-public class Payment {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class Payment {
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			// Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/paf_ca", "root", "root");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
 }

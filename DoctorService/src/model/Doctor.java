bfd418fde1661494ea4f5a5a34644c005d78611a
Piumi <piumi.sulochana1212@gmail.com>
2020-04-18 13:11:57 +0530

    Added database connection

--- DoctorService/src/model/Doctor.java
+++ DoctorService/src/model/Doctor.java
@@ -1,5 +1,31 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Time;
+
 public class Doctor {
-	//comment
+	
+	public Connection connect() {
+		Connection con = null;
+
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+			con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/hospitaldb", "root", "");	
+			System.out.print("Successfully connected");
+
+		} catch (Exception e) {
+
+			System.out.print("connection fail");
+			e.printStackTrace();
+			System.out.print(e);
+		}
+
+		return con;
+	}
 }

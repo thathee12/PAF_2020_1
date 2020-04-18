692b6f6d35023762d0d86b9f03e7b47148707a32
Jathushan <jathu.js1@gmail.com>
2020-04-18 17:20:37 +0530

    database connection

--- PatientService/src/model/Patient.java
+++ PatientService/src/model/Patient.java
@@ -1,5 +1,18 @@
 package model;
+import java.sql.*;
 
 public class Patient {
+	 // A common method to connect to the DB
+	private Connection connect() {
+			Connection con = null;
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+	//Provide the correct details: DBServer/DBName, username, password
+				con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/hospital", "root", "");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return con;
+		}
 
 }

64dbdb261b1734851e3c7d417a98381304b8a580
Accash <accashr4@gmail.com>
2020-04-18 16:07:37 +0530

    insert method added

--- PaymentService/src/model/Payment.java
+++ PaymentService/src/model/Payment.java
@@ -2,6 +2,9 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 public class Payment {
 	private Connection connect() {
@@ -14,5 +17,36 @@ public class Payment {
 			e.printStackTrace();
 		}
 		return con;
+	}
+
+	public String insertPayment(String name, String type, String bank, String tot)
+
+	{
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for inserting.";
+			}
+			// create a prepared statement
+			String query = " insert into payments(`p_id`,`NameOnCard`,`cardType`,`bank`,`totAmount`)"
+					+ " values (?, ?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, name);
+			preparedStmt.setString(3, type);
+			preparedStmt.setString(4, bank);
+			preparedStmt.setDouble(5, Double.parseDouble(tot));
+
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Inserted successfully";
+		} catch (Exception e) {
+			output = "Error while inserting the item.";
+			System.err.println(e.getMessage());
+		}
+		return output;
 	}
 }
